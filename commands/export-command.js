651c8b0575877ba74a9243f3da0ef42360a5d676
Adrian Kuper <adrian.kuper@hotmail.de>
2019-03-29 16:21:16 +0100

    export command now creates .csv file that contains one line for each day of the month

--- commands/export-command.js
+++ commands/export-command.js
@@ -6,15 +6,25 @@ const fs = require('fs');
 exports.attach = function(program) {
   program
     .command('export <p>')
+    .option('-m, --month <m>', 'Month of data file to be exported. Defaults to current month')
+    .option('-y, --year <y>', 'Year of data file to be exported. Defaults to current year')
     .description('todo')
     .action(function (p, cmd) {
       const fileData = dataFile.read();
       const data = JSON.parse(fileData);
       let exportFileContents = '';
 
-      data.forEach((entry) => {
-        exportFileContents += `${dayjs(entry.begin).format('HH:mm')},${dayjs(entry.end).format('HH:mm')}\n`
-      });
+      const lastDayOfMonth = dayjs().endOf('month').date();
+
+      for (let i = 1; i <= lastDayOfMonth; i++) {
+        let current = data.find((e) => e.day == i);
+        if (current) {
+          exportFileContents += `${dayjs(current.begin).format('HH:mm')},${dayjs(current.end).format('HH:mm')}\n`
+        } else {
+          exportFileContents += ',\n'
+        }
+      }
+
       const exportFileLocation = path.join(p, `${dayjs().format('MM-YYYY')}.csv`);
 
       fs.writeFileSync(exportFileLocation, exportFileContents);
