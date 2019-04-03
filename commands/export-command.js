33231a3ce9413554337a8426055a01ee4349b1aa
Adrian Kuper <adrian.kuper@hotmail.de>
2019-04-03 18:22:02 +0200

    Pimped the export command

--- commands/export-command.js
+++ commands/export-command.js
@@ -3,6 +3,50 @@ const dataFile = require('../util/data-file');
 const path = require('path');
 const fs = require('fs');
 
+function getWorkTime(begin, end) {
+  let minutes = end.diff(begin, 'minute');
+  if (minutes / 60 > 6) {
+    minutes -= 30;
+  }
+  if (minutes / 60 > 9) {
+    minutes -= 45;
+  }
+  console.log(minutes);
+  return minutes;
+}
+
+function getOvertime(workTime) {
+  return Math.max(0, workTime - (60 * 8));
+}
+
+function getUndertime(workTime) {
+  return Math.min(0, workTime - (60 * 8));
+}
+
+function buildLine(begin, end) {
+  let line = '';
+
+  line += `${begin.format('HH:mm')},`;
+
+  if (end) {
+    const workTimeInMinutes = getWorkTime(begin, end);
+    const workTimeHours = workTimeInMinutes / 60;
+    const overtime = getOvertime(workTimeInMinutes);
+    const undertime = getUndertime(workTimeInMinutes);
+
+    line += `${end.format('HH:mm')},`;
+    line += `${workTimeHours},`;
+    line += `${overtime ? (overtime / 60) : ''},`;
+    line += `${undertime ? (undertime / 60) : ''}`;
+  } else {
+    line += ',,,';
+  }
+  
+  line += '\n';
+
+  return line;
+}
+
 exports.attach = function(program) {
   program
     .command('export <p>')
@@ -19,9 +63,13 @@ exports.attach = function(program) {
       for (let i = 1; i <= lastDayOfMonth; i++) {
         let current = data.find((e) => e.day == i);
         if (current) {
-          exportFileContents += `${dayjs(current.begin).format('HH:mm')},${dayjs(current.end).format('HH:mm')}\n`
+          const begin = dayjs(current.begin);
+          const end = current.end ? dayjs(current.end) : null;
+          
+
+          exportFileContents += buildLine(begin, end);
         } else {
-          exportFileContents += ',\n'
+          exportFileContents += ',,,,\n'
         }
       }
 
