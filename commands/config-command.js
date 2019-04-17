7de114f23a419b2419636f9952ec009557c4b804
Adrian Kuper <adrian.kuper@hotmail.de>
2019-04-17 20:52:16 +0200

    Added list option to config command

--- commands/config-command.js
+++ commands/config-command.js
@@ -5,13 +5,19 @@ const CONFIG_FILE_NAME = '../time-log.config.json';
 
 exports.attach = function attach(program) {
   program
-    .command('config <name> <value>')
-    .action((name, value) => {
+    .command('config [name] [value]')
+    .option('-l, --list', 'List current config')
+    .action((name, value, cmd) => {
       const configFilePath = path.join(__dirname, CONFIG_FILE_NAME);
       const configData = fs.readFileSync(configFilePath);
       const config = JSON.parse(configData);
 
-      config[name] = value;
-      fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2));
+      if (name && value) {
+        config[name] = value;
+        fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2));
+      }
+      if (cmd.list) {
+        console.log(JSON.stringify(config, null, 2));
+      }
     });
 };
