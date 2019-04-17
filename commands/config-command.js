00d268f74536b82ce6444350c16f5eb053461a6d
Adrian Kuper <adrian.kuper@hotmail.de>
2019-04-17 20:45:28 +0200

    Added basic functionality to config command

--- commands/config-command.js
+++ commands/config-command.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const CONFIG_FILE_NAME = '../time-log.config.json';
+
 exports.attach = function attach(program) {
   program
     .command('config <name> <value>')
     .action((name, value) => {
-      console.log(`TODO: setting ${name} to ${value}`);
+      const configFilePath = path.join(__dirname, CONFIG_FILE_NAME);
+      const configData = fs.readFileSync(configFilePath);
+      const config = JSON.parse(configData);
+
+      config[name] = value;
+      fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2));
     });
 };
